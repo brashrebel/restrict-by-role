05f645863a17e92f9acb935408f11d66e281f477
Kyle <kyle@realbigmarketing.com>
2014-03-06 13:31:22 -0500

    First commit
    Starting this simple plugin.

---
+++ restrict-by-role.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Restrict By Role
+Description: A simple system for restricting access to front-end content by user role.
+Author: Kyle Maurer
+Version: 0.1
+Author URI: http://realbigmarketing.com
+*/
+function rbr() {
+	return 'oh happy days';
+}
+add_shortcode( 'rbr', 'rbr' );
+?>
